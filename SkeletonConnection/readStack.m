a45ccc6012eb6450601555304054b2c8f0c6f27e
Jeffrey Frey <frey@udel.edu>
2022-09-16 16:37:18 -0400

    [BUG] readStack(arg1, arg2) does not match with usage in ConnectSkeleton.m; removed data_folder argument

--- SkeletonConnection/readStack.m
+++ SkeletonConnection/readStack.m
@@ -1,4 +1,4 @@
-function [ slices ] = readStack(filename, data_folder)
+function [ slices ] = readStack(filename)
 %Checks is MIP/filename exists in MIP_folder, if so, load it, if not, do a maximum
 %intensity projection and return in
 %Loads whole thing in memory 
@@ -11,14 +11,13 @@ disp('Reading 3D Stack');
 %     return;
 % end
 
-fname = [data_folder filename];
-info = imfinfo(fname);
+info = imfinfo(filename);
 num_images = numel(info);
-slice_one = imread(fname, 1, 'Info', info);
+slice_one = imread(filename, 1, 'Info', info);
 slices = zeros(size(slice_one,1), size(slice_one, 2), num_images);
 slices(:,:,1) = slice_one;
 for k = 2:num_images
-    slices(:,:,k) = imread(fname, k, 'Info', info);
+    slices(:,:,k) = imread(filename, k, 'Info', info);
 end
 slices = uint8(slices);
 
